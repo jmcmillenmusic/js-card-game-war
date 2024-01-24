5f1f486b597d314de596b5f825e72408bdb5a3ae
Jeff McMillen <jmcmillenmusic@gmail.com>
2024-01-23 18:30:59 -0600

    Set up initial functionality to shuffle and deal cards to player and computer (back end)

---
+++ script.js
@@ -0,0 +1,40 @@
+// Initializes the entire deck, your deck, and the computer's deck as empty arrays
+const fullDeck = [];
+const playerDeck = [];
+const computerDeck = [];
+
+// Establishes suits and values of cards in a standard 52-card deck
+const suits = ["clubs", "diamonds", "hearts", "spades"];
+const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+
+// Creates and shuffles the deck using the Fisher-Yates Algorithm
+function shuffle() {
+    // Deck creation
+    for (let i = 0; i < suits.length; i++) {
+        for (let j = 0; j < values.length; j++) {
+            let card = {Value: values[j], Suit: suits[i]};
+            fullDeck.push(card);
+        }
+    }
+    // Deck shuffling
+    for (let i = fullDeck.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [fullDeck[i], fullDeck[j]] = [fullDeck[j], fullDeck[i]];
+    }
+    // Deal out all cards to you and the computer
+    for (let i = fullDeck.length; i > 0; i--) {
+        if (playerDeck.length === computerDeck.length) {
+            playerDeck.push(fullDeck[0]);
+            fullDeck.shift(fullDeck[0]);
+        } else {
+            computerDeck.push(fullDeck[0]);
+            fullDeck.shift(fullDeck[0]);
+        }
+    }
+    console.log(playerDeck);
+    console.log(computerDeck);
+    // Prevents you from drawing before you shuffle or shuffling after you draw
+    document.getElementById("playercards").style.visibility = "visible";
+    document.getElementById("computercards").style.visibility = "visible";
+    document.getElementById("fulldeck").style.visibility = "hidden";
+}
