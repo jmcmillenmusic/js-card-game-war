737a9b80ef51957e7a09ace399c7e401d4d14b2c
Jeff McMillen <jmcmillenmusic@gmail.com>
2024-01-27 09:31:51 -0600

    Dynamically creats card when the player plays a card

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ const fullDeck = [];
 const playerDeck = [];
 const computerDeck = [];
 
+// Initializes empty arrays to store the cards being played
+const playerPlay = [];
+const computerPlay = [];
+
 // Establishes suits and values of cards in a standard 52-card deck
-const suits = ["clubs", "diamonds", "hearts", "spades"];
-const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+const suits = ['clubs', 'diamonds', 'hearts', 'spades'];
+const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
 // Creates and shuffles the deck using the Fisher-Yates Algorithm
 function shuffle() {
@@ -34,13 +38,65 @@ function shuffle() {
     console.log(playerDeck);
     console.log(computerDeck);
     // Shows that cards have been dealt
-    document.getElementById("playercards").style.visibility = "visible";
-    document.getElementById("computercards").style.visibility = "visible";
-    document.getElementById("fulldeck").style.visibility = "hidden";
+    document.getElementById('playercards').style.visibility = 'visible';
+    document.getElementById('computercards').style.visibility = 'visible';
+    document.getElementById('fulldeck').style.visibility = 'hidden';
     // Prevents you from shuffling and dealing again
-    document.getElementById("shuffle").disabled = true;
-    document.getElementById("playButton").disabled = false;
+    document.getElementById('shuffle').disabled = true;
+    document.getElementById('playButton').disabled = false;
     // Shows the number of cards in your deck and the computer's deck
-    document.getElementById("playerCardCount").innerText = playerDeck.length;
-    document.getElementById("computerCardCount").innerText = computerDeck.length;
+    document.getElementById('playerCardCount').innerText = playerDeck.length;
+    document.getElementById('computerCardCount').innerText = computerDeck.length;
+}
+
+function play() {
+    // Plays your first card and the computer's first card
+    if (playerDeck.length > 0 && computerDeck.length > 0) {
+        playerPlay.push(playerDeck[0]);
+        playerDeck.shift(playerDeck[0]);
+        computerPlay.push(computerDeck[0]);
+        computerDeck.shift(computerDeck[0]);
+    }
+    console.log(playerPlay);
+    console.log(computerPlay);
+    console.log(playerDeck.length, computerDeck.length);
+    // Prevents you from clicking the Play button before the play is resolved
+    document.getElementById('playButton').disabled = true;
+    // Creates a container div with the fullcard class under playerCardArea
+    const playerCardArea = document.getElementById('playercard');
+    const cardContainer = document.createElement('div');
+    cardContainer.setAttribute('class', 'fullcard');
+    playerCardArea.appendChild(cardContainer);
+    // Creates the card image with the cardImg class under cardContainer
+    var cardImg = document.createElement('img');
+    cardImg.setAttribute('class', 'cardImg');
+    cardImg.src = 'images/blank_card.png';
+    cardContainer.appendChild(cardImg);
+    // Creates the card text with the cardText class under cardContainer
+    var cardText = document.createElement('h3');
+    cardText.setAttribute("class", "cardText");
+    cardContainer.appendChild(cardText);
+    // Sets the card value, suit, and text color
+    for (i = 0; i < playerPlay.length; i++) {
+        var card = playerPlay[i];
+        cardContainer.id = `${card.Value} of ${card.Suit}`;
+        switch (true) {
+            case (card.Suit === "clubs"):
+                cardText.textContent = `${card.Value}\u2663`;
+                cardText.style.color = "black";
+                break;
+            case (card.Suit === "diamonds"):
+                cardText.textContent = `${card.Value}\u2666`;
+                cardText.style.color = "red";
+                break;
+            case (card.Suit === "hearts"):
+                cardText.textContent = `${card.Value}\u2665`;
+                cardText.style.color = "red";
+                break;
+            case (card.Suit === "spades"):
+                cardText.textContent = `${card.Value}\u2660`;
+                cardText.style.color = "black";
+                break;
+        }
+    }
 }
