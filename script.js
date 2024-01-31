115effd7ee2fe6cea184c0e05dc1bb7d1a349e3c
Jeff McMillen <jmcmillenmusic@gmail.com>
2024-01-30 21:21:13 -0600

    Reset scripts temporarily so that all functions are on one script

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Import createCard() from createCard.js
-import createCard from './createCard.js';
+// import createCard from './createCard.js';
 
 // Initializes the entire deck, your deck, and the computer's deck as empty arrays
 const fullDeck = [];
@@ -23,11 +23,13 @@ function shuffle() {
             fullDeck.push(card);
         }
     }
+
     // Deck shuffling
     for (let i = fullDeck.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1));
       [fullDeck[i], fullDeck[j]] = [fullDeck[j], fullDeck[i]];
     }
+
     // Deal out all cards to you and the computer
     for (let i = fullDeck.length; i > 0; i--) {
         if (playerDeck.length === computerDeck.length) {
@@ -40,13 +42,16 @@ function shuffle() {
     }
     console.log(playerDeck);
     console.log(computerDeck);
+
     // Shows that cards have been dealt
     document.getElementById('playercards').style.visibility = 'visible';
     document.getElementById('computercards').style.visibility = 'visible';
     document.getElementById('fulldeck').style.visibility = 'hidden';
+    
     // Prevents you from shuffling and dealing again
     document.getElementById('shuffle').disabled = true;
-    document.getElementById('playButton').disabled = false;
+    document.getElementById('play').disabled = false;
+    
     // Shows the number of cards in your deck and the computer's deck
     document.getElementById('playerCardCount').innerText = playerDeck.length;
     document.getElementById('computerCardCount').innerText = computerDeck.length;
@@ -57,6 +62,10 @@ const shuffleButton = document.getElementById('shuffle');
 shuffleButton.addEventListener("click", shuffle);
 
 function play() {
+    // Initializes empty arrays to store the cards being played
+    const playerPlay = [];
+    const computerPlay = [];
+
     // Plays your first card and the computer's first card
     if (playerDeck.length > 0 && computerDeck.length > 0) {
         playerPlay.push(playerDeck[0]);
@@ -67,48 +76,55 @@ function play() {
     console.log(playerPlay);
     console.log(computerPlay);
     console.log(playerDeck.length, computerDeck.length);
+
     // Prevents you from clicking the Play button before the play is resolved
-    document.getElementById('playButton').disabled = true;
-    createCard();
-    // // Creates a container div with the fullcard class under playerCardArea
-    // const playerCardArea = document.getElementById('playercard');
-    // const cardContainer = document.createElement('div');
-    // cardContainer.setAttribute('class', 'fullcard');
-    // playerCardArea.appendChild(cardContainer);
-    // // Creates the card image with the cardImg class under cardContainer
-    // var cardImg = document.createElement('img');
-    // cardImg.setAttribute('class', 'cardImg');
-    // cardImg.src = 'images/blank_card.png';
-    // cardContainer.appendChild(cardImg);
-    // // Creates the card text with the cardText class under cardContainer
-    // var cardText = document.createElement('h3');
-    // cardText.setAttribute("class", "cardText");
-    // cardContainer.appendChild(cardText);
-    // // Sets the card value, suit, and text color
-    // for (i = 0; i < playerPlay.length; i++) {
-    //     var card = playerPlay[i];
-    //     cardContainer.id = `${card.Value} of ${card.Suit}`;
-    //     switch (true) {
-    //         case (card.Suit === "clubs"):
-    //             cardText.textContent = `${card.Value}\u2663`;
-    //             cardText.style.color = "black";
-    //             break;
-    //         case (card.Suit === "diamonds"):
-    //             cardText.textContent = `${card.Value}\u2666`;
-    //             cardText.style.color = "red";
-    //             break;
-    //         case (card.Suit === "hearts"):
-    //             cardText.textContent = `${card.Value}\u2665`;
-    //             cardText.style.color = "red";
-    //             break;
-    //         case (card.Suit === "spades"):
-    //             cardText.textContent = `${card.Value}\u2660`;
-    //             cardText.style.color = "black";
-    //             break;
-    //     }
-    // }
+    document.getElementById('play').disabled = true;
+    // createCard();
+
+    // Creates a container div with the fullcard class under playerCardArea
+    const playerCardArea = document.getElementById('playercard');
+    const cardContainer = document.createElement('div');
+    cardContainer.setAttribute('class', 'fullcard');
+    playerCardArea.appendChild(cardContainer);
+
+    // Creates the card image with the cardImg class under cardContainer
+    var cardImg = document.createElement('img');
+    cardImg.setAttribute('class', 'cardImg');
+    cardImg.src = 'images/blank_card.png';
+    cardContainer.appendChild(cardImg);
+
+    // Creates the card text with the cardText class under cardContainer
+    var cardText = document.createElement('h3');
+    cardText.setAttribute("class", "cardText");
+    cardContainer.appendChild(cardText);
+    
+    // Sets the card value, suit, and text color
+    for (let i = 0; i < playerPlay.length; i++) {
+        var card = playerPlay[i];
+        cardContainer.id = `${card.Value} of ${card.Suit}`;
+        switch (true) {
+            case (card.Suit === "clubs"):
+                cardText.textContent = `${card.Value}\u2663`;
+                cardText.style.color = "black";
+                break;
+            case (card.Suit === "diamonds"):
+                cardText.textContent = `${card.Value}\u2666`;
+                cardText.style.color = "red";
+                break;
+            case (card.Suit === "hearts"):
+                cardText.textContent = `${card.Value}\u2665`;
+                cardText.style.color = "red";
+                break;
+            case (card.Suit === "spades"):
+                cardText.textContent = `${card.Value}\u2660`;
+                cardText.style.color = "black";
+                break;
+        }
+    }
 }
 
 // Adds event listeners to the Play button instead of onclick() functions in index.html
 const playButton = document.getElementById('play');
 playButton.addEventListener('click', play);
+
+// export default {playerPlay, computerPlay};
