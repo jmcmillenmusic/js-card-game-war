ba25ab0ce147b22f4a37111814b9739747d23db0
Jeff McMillen <jmcmillenmusic@gmail.com>
2024-02-16 23:15:06 -0600

    Working on war function, card images not working just yet

--- script.js
+++ script.js
@@ -76,27 +76,30 @@ function play() {
     // Prevents you from clicking the Play button before the play is resolved
     document.getElementById('play').disabled = true;
     // createCard();
+}
 
+function createCards() {
+    // Creates card images based on the last card you and the computer played
     // TODO: Make these 2 for loops into a singular for loop if possible
     for (let i = 0; i < playerPlay.length; i++) {
         const playerCardArea = document.getElementById('playercard');
-
+    
         // Creates a container div with the fullcard class under playerCardArea
         const cardContainer = document.createElement('div');
         cardContainer.setAttribute('class', 'fullcard');
         playerCardArea.appendChild(cardContainer);
-
+    
         // Creates the card image with the cardImg class under cardContainer
         const cardImg = document.createElement('img');
         cardImg.setAttribute('class', 'cardImg');
         cardImg.src = 'images/blank_card.png';
         cardContainer.appendChild(cardImg);
-
+    
         // Creates the card text with the cardText class under cardContainer
         const cardText = document.createElement('h3');
         cardText.setAttribute("class", "cardText");
         cardContainer.appendChild(cardText);
-
+    
         // Sets the card value, suit, and text color
         const card = playerPlay[i];
         cardContainer.id = `${card.Value} of ${card.Suit}`;
@@ -119,26 +122,26 @@ function play() {
                 break;
         }
     }
-
+    
     for (let i = 0; i < computerPlay.length; i++) {
         const computerCardArea = document.getElementById('computercard');
-
+    
         // Creates a container div with the fullcard class under computerCardArea
         const cardContainer = document.createElement('div');
         cardContainer.setAttribute('class', 'fullcard');
         computerCardArea.appendChild(cardContainer);
-
+    
         // Creates the card image with the cardImg class under cardContainer
         const cardImg = document.createElement('img');
         cardImg.setAttribute('class', 'cardImg');
         cardImg.src = 'images/blank_card.png';
         cardContainer.appendChild(cardImg);
-
+    
         // Creates the card text with the cardText class under cardContainer
         const cardText = document.createElement('h3');
         cardText.setAttribute("class", "cardText");
         cardContainer.appendChild(cardText);
-
+    
         // Sets the card value, suit, and text color
         const card = computerPlay[i];
         cardContainer.id = `${card.Value} of ${card.Suit}`;
@@ -219,6 +222,7 @@ function compare() {
 // Adds event listeners to the Play button instead of onclick() functions in index.html
 const playButton = document.getElementById('play');
 playButton.addEventListener('click', play);
+playButton.addEventListener('click', createCards);
 playButton.addEventListener('click', compare);
 
 // Lets you take the cards that you and the computer play if you win
@@ -306,5 +310,37 @@ function give() {
 // Adds an event listener to the Take button instead of onclick() functions in index.html
 const giveButton = document.getElementById('give');
 giveButton.addEventListener('click', give);
+
+function war() {
+    // Plays the next 4 cards from both decks if they both have enough cards
+    if (playerDeck.length >= 4 && computerDeck.length >= 4) {
+        for (let i = 0; i < 4; i++) {
+            playerPlay.push(playerDeck[0]);
+            playerDeck.shift(playerDeck[0]);
+            computerPlay.push(computerDeck[0]);
+            computerDeck.shift(computerDeck[0]);
+        }
+    } else if (playerDeck.length < 4) {
+        // Plays all of your cards if you have fewer than 4 cards
+        for (let i = 0; i < playerDeck.length; i++) {
+            playerPlay.push(playerDeck[0]);
+            playerDeck.shift(playerDeck[0]);
+        }
+    } else if (computerDeck.length < 4) {
+        // Plays all of the computer's cards if it has fewer than 4 cards
+        for (let i = 0; i < computerDeck.length; i++) {
+            computerPlay.push(computerDeck[0]);
+            computerDeck.shift(computerDeck[0]);
+        }
+    }
+    console.log(playerPlay);
+    console.log(computerPlay);
+}
+
+// Adds an event listener to the War button instead of onclick() functions in index.html
+const warButton = document.getElementById('war');
+warButton.addEventListener('click', war);
+warButton.addEventListener('click', createCards);
+warButton.addEventListener('click', compare);
 
 // export default {playerPlay, computerPlay};
